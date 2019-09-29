fe8d9373f31d51d98f9094beb38d0f1965d041b6
Liu Maumasi <liu.maumasi@gmail.com>
2019-09-29 16:12:41 -0500

    hot fix: code error

--- src/lib/_gitInfo.js
+++ src/lib/_gitInfo.js
@@ -4,10 +4,8 @@
 const shell = require('shelljs');
 
 const gitInfo = {
-  return {
     user: shell.exec('`which git` config --list | grep "user.name"').split('=')[1].split('\n')[0],
     currentBranch: shell.exec('`which git` branch | `which grep` "*"').stdout.split(' ')[1].split('\n')[0],
-  }
 };
 
 module.exports = gitInfo;
